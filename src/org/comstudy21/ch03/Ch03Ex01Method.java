17bcd9c293693531e905d1379a66241540e9391f
Jinseob Kim <rlawlstjq343@naver.com>
2021-01-21 14:44:00 +0900

    point 객체 swap

--- src/org/comstudy21/ch03/Ch03Ex01Method.java
+++ src/org/comstudy21/ch03/Ch03Ex01Method.java
@@ -1,9 +1,23 @@
 package org.comstudy21.ch03;
 
 public class Ch03Ex01Method {
+	
+	static void swap(int x, int y) {
+		int t = x;
+		x = y;
+		y = t;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("123");
-		hub edit1 result?
-				backup confirm 1
+		System.out.println("call by value example");
+		// a -> x '값'이 전달 되는 것! 변수 자체가 전달 되는 것이 아니다.
+		// 즉, 함수 내에서 아무리 돌려도 밖의 값이 변경되는 것이 아니다! 서로 다른 값!
+		
+		int a = 100;
+		int b = 100; 
+		
+		swap(a, b);
+		System.out.println("a: "+a+", b: "+b);
+		
 	}
 }
---
+++ src/org/comstudy21/ch03/Ch03Ex02Refer.java
@@ -0,0 +1,39 @@
+package org.comstudy21.ch03;
+
+	// class는 사용자 정의 타입이다.
+	// 사용자가 임의로 만들어서 쓰는 데이터 타입
+	// 클래스 = (멤버)필드 + 메소드
+	// 필드 : 속성(변수)
+	// 메소드 : 일(함수)
+
+class Point {
+	// 포인트라는 객체를 만들어서 x,y를 집어넣었다.
+	// 어찌보면 배열과 비슷하다고 할 수 있다.
+	int x;
+	int y;
+}
+
+public class Ch03Ex02Refer {
+	
+	static void swap(Point p) { // 받을 땐 타입만 맞춰주면 됨!
+		int temp = p.x;
+		p.x = p.y;
+		p.y = temp;
+	
+	}
+	
+	public static void main(String[] args) {
+		
+		Point p1 = null; // Point 타입의 참조변수 p1을 선언했다.
+		p1 = new Point(); // new라는 연산자를 써야 객체든 뭐든 만들 수 있다. 
+		//p1=레퍼런스!!!
+		System.out.println(p1);
+		
+		p1.x = 100;
+		p1.y = 200;
+		
+		swap(p1);
+		System.out.println("p.x: "+p1.x+", p.1y; "+p1.y);
+	}
+
+}
